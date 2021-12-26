e6565c4089a0720130f4c029f0ed223bd5267bf6
George S. Hugh <hughgs@duke.edu>
2021-12-26 10:54:43 -0500

    Add tab items.

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,5 +1,15 @@
 <template>
-    <h1> Navigation Bar </h1>
+    <v-tabs>
+        <v-tab ripple> Team Database </v-tab>
+        <v-tab-item> Put team database here </v-tab-item>
+
+        <v-tab ripple> Team Mods </v-tab>
+        <v-tab-item> Put team mods application here </v-tab-item>
+
+        <v-tab ripple> Defense Mods </v-tab>
+        <v-tab-item> Put defense mods application here </v-tab-item>
+    </v-tabs>
+
 </template>
 
 <script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,10 @@
+<!-- Opening page -->
 <template>
   <Navbar />
 </template>
 
 <script>
-  import Navbar from '../components/Navbar'
+  import Navbar from '@/components/Navbar'
 
   export default {
     name: 'Home',
