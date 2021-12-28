64f13c5f64d7443b9a1da5d6d5c215aae260121f
George S. Hugh <hughgs@duke.edu>
2021-12-28 15:53:29 -0500

    Add some comments and export component names.

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -3,11 +3,11 @@
         <v-text-field class="shrink mx-4" prefix="Ally Code"
             single-line outlined counter=9></v-text-field>
         <v-tabs slot="extension">
-            <v-tab v-for="tab in tabs" :key="tab.title">
+            <v-tab v-for="tab in tabs" :key="tab">
                 {{tab.title}}
             </v-tab>
 
-            <v-tab-item v-for="tab in tabs" :key="tab.comp">
+            <v-tab-item v-for="tab in tabs" :key="tab">
                 <component :is="tab.comp"> </component>
             </v-tab-item>
         </v-tabs>
@@ -20,16 +20,20 @@
     import DefModApp from '@/components/DefModApp'
 
     export default {
+        // What is this component
         name: 'NavBar',
 
+        // What components does this use
         components: {
           ModDBApp,
           TeamModApp,
           DefModApp
         },
 
+        // What kind of data is needed internally
         data () {
             return {
+                // Entries dictate order of tabs
                 tabs: [
                     { title: 'Team Database', comp: ModDBApp },
                     { title: 'Team Mods', comp: TeamModApp },
--- frontend/src/components/TeamModApp.vue
+++ frontend/src/components/TeamModApp.vue
@@ -4,6 +4,7 @@
 
 <script>
     export default {
+        name: 'TeamModApp',
     
     }
 </script>
