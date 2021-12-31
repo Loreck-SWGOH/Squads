3cb729415b7831f30d04c83f909d2b9d353eb3ee
George S. Hugh <hughgs@duke.edu>
2021-12-30 21:38:10 -0500

    Limit number of characters

--- frontend/src/components/AllyCode.vue
+++ frontend/src/components/AllyCode.vue
@@ -1,6 +1,6 @@
 <template>
     <v-text-field class="shrink ma-2" prefix="Ally Code"
-        single-line outlined counter=11
+        single-line outlined counter=11 maxlength=11
         v-model="allyCodeNum"></v-text-field>
 </template>
 
@@ -22,14 +22,14 @@
 
         watch: {
             allyCodeNum() {
-                // Card number without dash (-)
+                // Ally code without dash (-)
                 let realNumber = this.allyCodeNum.replace(/-/gi, '')
         
                 // Generate dashed number
                 let dashedNumber = realNumber.match(/.{1,3}/g)
 
-                // Replace the dashed number with the real one
-                this.allyCodeNum = dashedNumber.join('-')                
+                // Replace dashed number with real one
+                this.allyCodeNum = dashedNumber.join('-')
             }
         }
     }
