93c351758244758f9c84b4453873f5d1f970c0a9
George S. Hugh <hughgs@duke.edu>
2022-01-18 17:09:58 -0500

    Create front page for mod database page.

--- frontend/src/components/ModDBApp.vue
+++ frontend/src/components/ModDBApp.vue
@@ -1,10 +1,38 @@
 <template>
-    <h1> Mod Database App Page </h1>
+    <v-container>
+        <ModDBIntro />
+        <v-list>
+        </v-list>
+    </v-container>
 </template>
 
 <script>
+    import ModDBIntro from '@/components/ModDBIntro'
+
     export default {
+        // Component name
         name: 'ModDBApp',
     
+        components: {
+            ModDBIntro,
+        },
+
+        // Local data
+        data () {
+            return {
+//                introModDB: 'Lorem'
+            }
+        },
+
+        computed: {
+            introModDB() {
+                var request = new XMLHttpRequest();
+                request.open('GET', '../assets/ModDBIntro.txt', false);
+                request.send();
+                return request.responseText;
+//                return 'Welcome ' + this.allyCodeName
+            }
+        },
+
     }
 </script>
